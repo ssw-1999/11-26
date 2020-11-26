c4d355509bce7a66299d8c262afb68ac868419d2
sunshuwei <>
2020-11-26 10:44:53 +0800

    p-3

---
+++ practise/my-project/src/components/new1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'new1',
+  data () {
+    return {
+      msg: 'Welcome to new1'
+    }
+  },
+//   methods:{
+//     getData:function(){
+//       this.axios.post(
+//         '/v3/acfun/crawl/content/condition',
+//         { }).then(function(res){
+//           console.log(res);
+//         })
+//     }
+//   },
+//   created(){
+//     this.getData();
+//   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
