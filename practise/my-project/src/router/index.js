3e58aaaaaa117a3cffbb0ecca258766cbacdc166
sunshuwei <>
2020-11-26 18:24:23 +0800

    submit with rebase

--- practise/my-project/src/router/index.js
+++ practise/my-project/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import new5 from '../components/new5'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/new5',
+      name:'new5',
+      component:new5
     }
   ]
 })
