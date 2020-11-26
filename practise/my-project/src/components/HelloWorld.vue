6d8907813efbc78a8a337fe35e57372d5e494cf2
sunshuwei <>
2020-11-26 14:10:26 +0800

    all

--- practise/my-project/src/components/HelloWorld.vue
+++ practise/my-project/src/components/HelloWorld.vue
@@ -1,17 +1,32 @@
 <template>
   <div>
-    {{msg}}
+    <ul>
+      <li>{{msg}}</li>
+      <li>{{fromVue1}}</li>
+    </ul>
+    <new1 @getData="updateData" :prop="fromVue1"></new1>
   </div>
 </template>
 
 <script>
+import new1 from './new1'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      fromVue1:''
     }
   },
+  components:{
+    new1
+  },
+  methods:{
+      updateData(data){
+          this.fromVue1=data;
+      }
+  }
   // methods:{
   //   getData:function(){
   //     this.axios.post(
@@ -29,5 +44,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+ul li{
+  list-style: none;
+}
 </style>
--- practise/my-project/src/components/new1.vue
+++ practise/my-project/src/components/new1.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    {{msg}}
-    {{msg}}
+      <ul>
+          <li>{{msg}}</li>
+          <li>
+              <input type="text" v-model="word1">
+              <button @click="sendData()">像父组件发送数据</button>
+          </li>
+          <li>{{prop}}</li>
+      </ul>
   </div>
 </template>
 
@@ -10,9 +16,18 @@ export default {
   name: 'new1',
   data () {
     return {
-      msg: 'Welcome to new1'
+      msg: 'Welcome to new1',
+      word1:""
     }
   },
+  props:{
+      prop:String
+  },
+  methods:{
+      sendData(){
+          this.$emit('getData',this.word1)
+      }
+  }
 //   methods:{
 //     getData:function(){
 //       this.axios.post(
@@ -30,5 +45,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+ul li{
+  list-style: none;
+}
 </style>
