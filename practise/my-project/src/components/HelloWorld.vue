edc1a700068c4cedcd8d8d96aff4be3a1589331e
sunshuwei <>
2020-11-26 10:39:25 +0800

    p-1

---
+++ practise/my-project/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    getData:function(){
+      this.axios.post(
+        '/v3/acfun/crawl/content/condition',
+        { }).then(function(res){
+          console.log(res);
+        })
+    }
+  },
+  created(){
+    this.getData();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
